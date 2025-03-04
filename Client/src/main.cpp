4f10439597215d9eda83c31073c85e970927631d
ardx3000 <75367071+ardx3000@users.noreply.github.com>
2025-03-04 22:39:41 +0000

    Project structure update

---
+++ Client/src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Is working " << std::endl;
+    return 0;
+}
---
+++ external scripts/compile.ps1
@@ -0,0 +1,5 @@
+#set paths
+
+$binDirectory = "bin"
+$clientSrc = "../Client/src/main.cpp"
+$serverSrc = "../Server/src/main.cpp"
